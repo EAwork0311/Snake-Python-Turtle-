0b2e813007a4dd14db325579f0d67a5bb59bba83
EAwork0311 <ea031109@gmail.com>
2026-05-15 12:58:38 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -18,44 +18,94 @@ def playing_area():
     pen.end_fill()
     
 class Head(Turtle):
-  def __init__(self, screen, body):
+  def __init__(self, screen, body,left_key,right_key,up_key,down_key):
     super().__init__()
-    pass
+    self.ht()
+    self.speed(0)
+    self.color("red")
+    self.penup()
+    self.setheading(90)
+    self.shape("square")
+    self.alive = True
+    self.st()
+    screen.onkeypress(self.left, left_key)
+    screen.onkeypress(self.right, right_key)
+    screen.onkeypress(self.up, up_key)
+    screen.onkeypress(self.down, down_key)
+    body.append(self)
 
   def up(self):
-    pass
+    self.setheading(90)
 
   def down(self):
-    pass
+    self.setheading(270)
 
   def left(self):
-    pass
+    self.setheading(180)
 
   def right(self):
-    pass
+    self.setheading(0)
+
+  def destroy(self):
+       self.hideturtle()
+       self.alive = False
 
   def move(self):
-    pass
+        self.forward(20)
+        if self.xcor() > 240 or self.xcor() < -240:
+          self.destroy()
+        if self.ycor() > 240 or self.ycor() < -240:
+            self.destroy()
+        
+        
+        
     
-  def die(self):
-    pass
 
 
 class Segment(Turtle):
   def __init__(self, other):
     super().__init__()
-    pass
+    self.ht()
+    self.speed(0)
+    self.color("green")
+    self.penup()
+    self.setheading(90)
+    self.shape("square")
+    self.alive = True
+    self.st()
+    
+    self.last = body[-1]
+    self.goto(self.last.xcor(),self.last.ycor())
 
-  def move(self, other):
-    pass
+  def move(self):
+     self.goto(self.last.xcor(),self.last.ycor())
+
+
 
 class Apple(Turtle):
   def __init__(self):
     super().__init__()
-    pass
+    self.penup()
+    self.speed(0)
+    self.color("red")
+    self.shape("circle")
+    self.goto(random.randint(-220,220),random.randint(-220,220))
 
-  def relocate(self):
-    pass
+  def relocate(self): 
+    self.goto(random.randint(-220,220),random.randint(-220,220))
+    body.append(Segment(p1))
+ 
+
+def update():
+  if p1.alive:
+    for i in range(len(body)-1,-1,-1):
+      body[i].move()
+    if p1.distance(apple) < 30:
+      apple.relocate()
+    for segment in body[3:len(body)-1:1]:
+      if segment.distance(p1) < 10:
+        p1.destroy() 
+  screen.ontimer(update, 120)
 
 screen = Screen()
 screen.bgcolor("black")
@@ -66,7 +116,13 @@ screen.listen()
 body = []
 
 
-screen.exitonclick()
+playing_area()
+
+p1 = Head(screen,body,"Left","Right","Up","Down" )
+
+apple = Apple()
+
+update()
 
 
 
